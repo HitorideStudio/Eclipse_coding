d9285e09582d2cd1d5eec0092257ffb3d5c6b2ae
HitorideStudio <mfboy19@gmail.com>
2019-07-01 17:07:23 +0900

    에러 수정

--- mvc/src/web/mvc/bean/SuperBean.java
+++ mvc/src/web/mvc/bean/SuperBean.java
@@ -2,12 +2,14 @@ package web.mvc.bean;
 
 import java.io.IOException;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.rowset.serial.SerialException;
+
 
 public interface SuperBean {
-	public String actionBean(HttpServletRequest request,HttpServletResponse response) throws SerialException, IOException;
+	public String actionBean(HttpServletRequest request,HttpServletResponse response) 
+			throws ServletException, IOException;
 	
 
 }
--- mvc/src/web/mvc/bean/loginFormBean.java
+++ mvc/src/web/mvc/bean/loginFormBean.java
@@ -2,13 +2,14 @@ package web.mvc.bean;
 
 import java.io.IOException;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.rowset.serial.SerialException;
+
 
 public class loginFormBean implements SuperBean{
 	public String actionBean(HttpServletRequest request, HttpServletResponse response)
-			throws SerialException, IOException{
+			throws ServletException, IOException{
 		return "/0701/loginForm.jsp";
 	}
 	
